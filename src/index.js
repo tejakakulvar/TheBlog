2d67a167c9c9f64eef5aab3a5866cc24408d0e7e
tejakakulvar <teja.kakulvar@gmail.com>
2017-11-21 20:31:03 -0600

    Commit 1

--- src/index.js
+++ src/index.js
@@ -2,14 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import App from './components/app';
 import reducers from './reducers';
 
+class First extends React.Component{
+  render(){
+    return <div>First</div>;
+  }
+}
+class Second extends React.Component{
+  render(){
+    return <div>Second</div>;
+  }
+}
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Route path="/First" component={First}/>
+        <Route path="/Second" component={Second}/>
+      </div>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
